07f3bbb586828c1f491b8324000374e122eabbae
fabian hirter <mail@fabianhirter.ch>
2024-12-06 15:50:24 +0100

    add terser

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: true,
+      mangle: true,
+      keep_classnames: false,
+      keep_fnames: true,
+      toplevel: false,
+    }
+  },
 })
